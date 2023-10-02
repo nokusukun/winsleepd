071c36d402287cd47927b41fe75466ea60cad5cd
Noku <noku@onlypa.ws>
2023-10-03 07:45:04 +0800

    Small update to Update to iterate through the TableKeyMap struct.

--- cmd/tui/cmd/update.go
+++ cmd/tui/cmd/update.go
@@ -3,17 +3,26 @@ package tui
 import (
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
+	"reflect"
 )
 
 func (m DaemonModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch {
-		case key.Matches(msg, m.TableKeyMap.GotoBottom), key.Matches(msg, m.TableKeyMap.GotoTop):
-			m.Table, _ = m.Table.Update(msg)
-			return m, nil
+		refKeyMap := reflect.ValueOf(m.TableKeyMap)
+		for i := 0; i < refKeyMap.NumField(); i++ {
+			keyMap := refKeyMap.Field(i).Interface().(key.Binding)
+			if key.Matches(msg, keyMap) {
+				m.Table, cmd = m.Table.Update(msg)
+				return m, cmd
+			}
 		}
+		//switch {
+		//case key.Matches(msg, m.TableKeyMap.GotoBottom), key.Matches(msg, m.TableKeyMap.GotoTop):
+		//	m.Table, _ = m.Table.Update(msg)
+		//	return m, nil
+		//}
 	default:
 		return m, cmd
 	}
